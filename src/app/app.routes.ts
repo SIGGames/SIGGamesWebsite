1bac2bb6b3340a2538ee097a322f5418c7c20874
Aniol <aniol0012@gmail.com>
2024-08-09 17:32:59 +0200

    init: project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { GamesComponent } from './components/games/games.component';
+import { GameDetailComponent } from './components/game-detail/game-detail.component';
+import { TeamComponent } from './components/team/team.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'games', component: GamesComponent },
+  { path: 'game/:id', component: GameDetailComponent },
+  { path: 'team', component: TeamComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'contact', component: ContactComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/components/games/games.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-games',
+  standalone: true,
+  imports: [],
+  templateUrl: './games.component.html',
+  styleUrl: './games.component.css'
+})
+export class GamesComponent {
+
+}
