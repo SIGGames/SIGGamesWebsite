6c6cf7e92aa86adf35e132f539450bd43efe554c
Aniol0012 <aniol0012@gmail.com>
2024-08-18 20:37:01 +0200

    feat: Update page title and remove logo from index.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppService } from './app.service';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private appService: AppService) {}
+  // Configurations
+  private readonly showCustomFocusTitle: boolean = true;
+  private readonly customFocusTitleSeconds: number = 2;
+  private readonly persistAltTitleOnBlur: boolean = true;
+
+  // Titles
+  private readonly originalTitle: string;
+  private readonly altTitle: string = 'SIG Games :(';
+  private readonly focusTitle: string = 'SIG Games :)';
+
+  constructor(private appService: AppService, private titleService: Title) {
+    this.originalTitle = this.appService.getAppName();
+    this.titleService.setTitle(this.originalTitle);
+  }
+
+  @HostListener('window:blur', [])
+  onWindowBlur() {
+    if (this.persistAltTitleOnBlur) {
+      this.titleService.setTitle(this.altTitle);
+    }
+  }
+
+  @HostListener('window:focus', [])
+  onWindowFocus() {
+    if (this.showCustomFocusTitle) {
+      this.titleService.setTitle(this.focusTitle);
+      setTimeout(() => {
+        this.titleService.setTitle(this.originalTitle);
+      }, this.customFocusTitleSeconds * 1000);
+    } else {
+      this.titleService.setTitle(this.originalTitle);
+    }
+  }
 
   getAppName(): string {
     return this.appService.getAppName();
