7df990924d61ffa363e84344372786b0876389b7
Aniol <aniol0012@gmail.com>
2024-08-24 20:59:40 +0200

    fix: Redirect user to home after form sent and improve code

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,18 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { AppService } from '../../app.service';
 import { FormsModule, NgForm } from '@angular/forms';
 import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule]
 })
 export class HomeComponent implements OnInit {
   appName: string = '';
 
-  constructor(private appService: AppService) {}
+  // Email Config
+  private emailServiceId: string = 'service_0wg0538';
+  private emailTemplateId: string = 'template_vff6j8p';
+  private emailUserId: string = 'mzm-0jmzxq72JQMlu';
+
+  constructor(private appService: AppService, private router: Router) { }
 
   ngOnInit(): void {
     this.appName = this.appService.getAppName();
@@ -32,21 +38,25 @@ export class HomeComponent implements OnInit {
       });
     }
   }
-
   sendEmail(form: NgForm) {
     if (form.valid) {
-      emailjs.send('service_0wg0538', 'template_vff6j8p', {
+      emailjs.send(this.emailServiceId, this.emailTemplateId, {
         firstName: form.value.firstName,
         lastName: form.value.lastName,
         email: form.value.email,
         message: form.value.message
-      }, 'mzm-0jmzxq72JQMlu')
+      }, this.emailUserId)
       .then((response: EmailJSResponseStatus) => {
-        console.log('Email sent successfully!', response.status, response.text);
+        this.sendNotification(`El correu s'ha enviat correctament`);
         form.resetForm();
       }, (error) => {
-        console.error('Failed to send email. Error:', error);
+        this.sendNotification(`No s'ha pogut enviar el correu correctament`);
       });
+      this.router.navigate(['/']);
     }
+  }
+
+  sendNotification(msg: string) {
+    alert(msg);
   }
 }
