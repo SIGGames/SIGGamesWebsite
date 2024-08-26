422f279e436fab14cb948fcbff6fdcf82b89ca15
Aniol0012 <aniol0012@gmail.com>
2024-08-26 23:20:00 +0200

    feat: add 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { GameDetailComponent } from './components/game-detail/game-detail.component';
 import { BlogComponent } from './components/blog/blog.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'game/:id', component: GameDetailComponent },
   { path: 'blog', component: BlogComponent },
+  { path: '**', component: NotFoundComponent }
 ];
