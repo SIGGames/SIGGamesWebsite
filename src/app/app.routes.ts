7cf535f277cb544aa2b1b46127f9ab1f4ebc515e
Aniol0012 <aniol0012@gmail.com>
2024-08-26 22:53:51 +0200

    chore: removed unused components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { GamesComponent } from './components/games/games.component';
 import { GameDetailComponent } from './components/game-detail/game-detail.component';
-import { TeamComponent } from './components/team/team.component';
 import { BlogComponent } from './components/blog/blog.component';
-import { ContactComponent } from './components/contact/contact.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'games', component: GamesComponent },
   { path: 'game/:id', component: GameDetailComponent },
-  { path: 'team', component: TeamComponent },
   { path: 'blog', component: BlogComponent },
-  { path: 'contact', component: ContactComponent },
 ];
