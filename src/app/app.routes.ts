1b0b0b4d0ba5387656fd751de9243028dcd627c2
Aniol <aniol0012@gmail.com>
2024-10-16 17:03:26 +0200

    feat: add newsletter component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { GameDetailComponent } from './components/game-detail/game-detail.component';
 import { BlogComponent } from './components/blog/blog.component';
+import { NewsletterComponent } from './components/newsletter/newsletter.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
@@ -9,5 +10,6 @@ export const routes: Routes = [
   { path: 'game/:id', component: GameDetailComponent },
   { path: 'blog', component: BlogComponent },
   { path: 'blog/:id', component: BlogComponent },
+  { path: 'newsletter/:id', component: NewsletterComponent },
   { path: '**', component: NotFoundComponent }
 ];
---
+++ src/app/components/newsletter/newsletter.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-newsletter',
+  standalone: true,
+  imports: [],
+  templateUrl: './newsletter.component.html',
+  styleUrl: './newsletter.component.css',
+  encapsulation: ViewEncapsulation.None
+})
+export class NewsletterComponent implements OnInit {
+  newsletterContent: string = '';
+  newsletterId: string = '';
+
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      const newsletterId = params.get('id');
+      if (newsletterId) {
+        this.loadNewsletterContent(newsletterId);
+      } else {
+        this.redirectNotFound();
+      }
+    });
+  }
+
+  loadNewsletterContent(newsletterId: string): void {
+    this.newsletterId = newsletterId;
+    const newsletterFile = `/assets/newsletter/newsletter-${newsletterId}.html`;
+
+    fetch(newsletterFile)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.text();
+      })
+      .then(content => {
+        this.newsletterContent = content;
+      })
+      .catch(error => {
+        this.redirectNotFound();
+      });
+  }
+
+  redirectNotFound(): void {
+    this.router.navigate(['/newsletter/' + this.newsletterId + '/not-found']);
+  }
+}
