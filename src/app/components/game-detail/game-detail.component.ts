4541e2a3a1c648f74a99668a4a11eeee32c6cef6
Aniol0012 <aniol0012@gmail.com>
2024-10-23 21:18:44 +0200

    feat: add provisional page on games

--- src/app/components/game-detail/game-detail.component.ts
+++ src/app/components/game-detail/game-detail.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-game-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent],
   templateUrl: './game-detail.component.html',
   styleUrl: './game-detail.component.css'
 })
