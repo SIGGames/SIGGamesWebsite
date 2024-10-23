7f62d3f3201aa5c23baa809779b63a01a57c3d1e
Aniol <aniol0012@gmail.com>
2024-10-23 15:22:44 +0200

    refactor: add newsletter css and fix game section

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,7 +9,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
+  styleUrls: ['./home.component.css', './newsletter.home.component.css'],
   standalone: true,
   imports: [FormsModule, HttpClientModule, CommonModule]
 })
