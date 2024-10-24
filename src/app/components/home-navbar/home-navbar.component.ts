1f8dc7c86f80c42b35cb58f1907f66bdaf68c54b
Aniol0012 <aniol0012@gmail.com>
2024-10-24 13:28:00 +0200

    fix: move footer on small res

--- src/app/components/home-navbar/home-navbar.component.ts
+++ src/app/components/home-navbar/home-navbar.component.ts
@@ -1,17 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home-navbar.component.html',
   styleUrl: './home-navbar.component.css'
 })
 export class HomeNavbarComponent {
+  isNearFooter: boolean = false;
+
   scrollToSection(sectionId: string) {
     const section = document.querySelector(sectionId);
     if (section) {
       section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const footer = document.querySelector('footer');
+    if (footer) {
+      const footerRect = footer.getBoundingClientRect();
+      this.isNearFooter = footerRect.top < window.innerHeight;
     }
   }
 }
